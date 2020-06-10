1f961beea04f70d6117928ff314f2682e039aa4c
Harshvardhan Jha <harshvardhanjha1@gmail.com>
2020-06-11 02:36:50 +0530

    Modified Search Components

--- client/src/components/utils/ItemList.jsx
+++ client/src/components/utils/ItemList.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import "./util.css";
 const ItemList = props => {
   return (
-    <ul className='courseSearch'>
+    <ul className='courseSearch' >
       {Object.keys(props.items).map(item => {
         return (
           <div
             className='courseItem'
+            style={{display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontSize: 14}}
             key={item}
             id={item}
             onClick={props.action}
--- client/src/components/utils/Search.jsx
+++ client/src/components/utils/Search.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import TextField from '@material-ui/core/TextField';
 
 const Search = props => {
   return (
-    <div className="input-field">
-      <input type="text" placeholder="Search Course" onChange={props.action} />
+    <div className="input-field"style={{padding:"5px"}}>
+      <TextField
+          id="standard-full-width"
+          label="Course"
+          placeholder="Search Course"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          onChange={props.action}
+        />
     </div>
   );
 };
